903f7ae6d9d5205cc4a195a03db9b38e912325b5
Jeffrey Nijs <jeffrey.nijs@outlook.com>
2022-07-12 23:53:41 +0200

    feat: pokemon list

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="12" md="6" sm="12" v-for="pokemon in poke" :key="pokemon.id">
+                <CardPokemon :poke="pokemon" />
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import CardPokemon from "@/components/CardPokemon.vue";
+export default {
+    props: { poke: Array },
+    name: "PokemonList",
+    components: {
+        CardPokemon,
+    },
+};
+</script>
+
+
+<style>
+</style>
