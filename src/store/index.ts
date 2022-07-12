be3d45f81c3430d8cce8cdef81462dd9a0572c43
Jeffrey Nijs <jeffrey.nijs@outlook.com>
2022-07-12 23:53:16 +0200

    feat: store api call

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,19 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    pokemon: [],
   },
   getters: {
   },
   mutations: {
+    SET_POKEMON(state, data) { state.pokemon = data; },
   },
   actions: {
+    async getPokemon({ commit }) {
+      await axios.get('https://stoplight.io/mocks/appwise-be/pokemon/57519009/pokemon').then(response => commit("SET_POKEMON", response.data));
+    }
   },
   modules: {
   }
